7504a09d1a3dd50094c7893149535f36a8e8a401
gyf5267 <gyf5267218@163.com>
2015-10-06 20:26:08 +0800

    Initial Commit

---
+++ TestApp123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp123
+//
+//  Created by nate on 15/10/6.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
